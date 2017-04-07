3b5fca7b9049cee9522e29709fb50f43b7b10b1a
Ashish Mittal <ashish.mittal@myoxigen.com>
2017-04-07 17:17:19 +0530

    Initial Commit

---
+++ LocationTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationTracker
+//
+//  Created by Ashish Mittal  on 07/04/17.
+//  Copyright © 2017 Ashish Mittal . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
